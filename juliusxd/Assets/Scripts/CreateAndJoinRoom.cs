934b19552a334c25e2d7f2024eb204003a2ce34d
Your Name <you@example.com>
2021-09-14 18:16:57 +0300

    Lobby melkein valmis

---
+++ juliusxd/Assets/Scripts/CreateAndJoinRoom.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+
+public class CreateAndJoinRoom : MonoBehaviourPunCallbacks
+
+{
+    public TMP_InputField createInput;
+    public TMP_InputField joinInput;
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+}
