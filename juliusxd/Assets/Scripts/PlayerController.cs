7961ea8d6a6e5eb2742e282686b5e25e6428c1ef
Your Name <you@example.com>
2021-09-28 18:15:09 +0300

    Playerin rakentaminen aloitettu

---
+++ juliusxd/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController controller;  
+
+    public float moveSpeed = 8f;
+
+    public float runSpeed = 14f;
+
+    public float jumpheight = 3f;
+
+    public float gravity = -9.81f;
+
+
+    public Vector3 velocity;
+
+    public Transform groundcheck;
+    
+    public float groundDistance = 0.1f;
+
+    public LayerMask groundMask; 
+
+
+
+
+    [SerializeField] private bool isGrounded;
+
+
+    private Vector3 move; 
+    // Start is called before the first frame update
+
+        void Start()
+        {
+        controller = GetComponent<CharacterController>();
+        }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIfGrounded(); 
+    }
+
+    private void CheckIfGrounded()
+    {
+
+    }
+}
+
